8fab83c8e3e66e0192354e8ae6fcceedc93bb636
luciodanielcv <lcastelan@gmail.com>
2021-04-25 21:26:48 -0500

    LDCV Hello world lambda function

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+console.log('Loading function');
+
+exports.handler = (event, context, callback) => {
+    callback(null, "Hello World! from Lambda AWS.");
+};
+
+//This simple javascript returns Hello World! to the requester.
+//Does nothing else.
+//
+//Method handler is responsible to catch requests to the Lambda function. Then the code within is executed.
+//
+//Callback parameter-function returns the Hello World string to the requester.
+//
